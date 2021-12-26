94a1cd1248039fd6cd2d787f638472691f8b07ff
caraquri-ykitahara <y.kitahara+fr@caraquri.com>
2021-12-26 16:20:51 +0900

    first commit

---
+++ MyLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLife
+//
+//  Created by 北原義久 on 2021/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
