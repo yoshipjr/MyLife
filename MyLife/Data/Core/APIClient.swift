3a773f72cc23c13a55cd34c809ce664e438a0e9b
yoshipjr <yoshihisa4413kitahara@gmail.com>
2021-12-26 21:11:37 +0900

    apierror 作成

---
+++ MyLife/Data/Core/APIClient.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+//struct APIClient {
+//    func request<R: Request, T>(
+//        item: R,
+//        useTestData: Bool = false,
+//        completion: @escaping (Result<T, >) -> Void
+//    ) {
+//
+//    }
+//}
---
+++ MyLife/Data/Core/APIError.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+public enum APIError: LocalizedError, Equatable {
+    case unknown
+    case missingTestJsonDataPath
+    case invalidRequest
+    case offline
+    case decodeError(String)
+    case reponseError
+
+    public var errorDescription: String? {
+        switch self {
+            case .unknown:
+                return "unknown error occured"
+
+            case .missingTestJsonDataPath:
+                return "missing test json data path"
+
+            case .invalidRequest:
+                return "invalid request"
+
+            case .offline:
+                return "offline error occured"
+
+            case let .decodeError(error):
+                return "decode error occured, \(error)"
+
+            case .reponseError:
+                return "response error occured"
+        }
+    }
+}
+
+extension DecodingError {
+    var context: DecodingError.Context? {
+        switch self {
+            case .typeMismatch(_, let context):
+                return context
+
+            case .valueNotFound(_, let context):
+                return context
+
+            case .keyNotFound(_, let context):
+                return context
+
+            case .dataCorrupted(let context):
+                return context
+
+            default:
+                return nil
+        }
+    }
+
+    var debugDescription: String {
+        return context?.debugDescription ?? ""
+    }
+
+    var key: String {
+        return context?.codingPath.map { $0.stringValue }.description ?? ""
+    }
+}
