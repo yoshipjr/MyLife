62bb30fa83613bdade10beae623f7af2e1ae90b8
yoshipjr <yoshihisa4413kitahara@gmail.com>
2021-12-26 21:14:58 +0900

    APIcliet作成

--- MyLife/Data/Core/APIClient.swift
+++ MyLife/Data/Core/APIClient.swift
@@ -1,11 +1,15 @@
 import Foundation
 
-//struct APIClient {
-//    func request<R: Request, T>(
-//        item: R,
-//        useTestData: Bool = false,
-//        completion: @escaping (Result<T, >) -> Void
-//    ) {
-//
-//    }
-//}
+struct APIClient {
+    func request<R: Request, T>(
+        item: R,
+        useTestData: Bool = false,
+        completion: @escaping (Result<T, APIError>) -> Void
+    ) where R.Response == T {
+        #if DEBUG
+        if useTestData {
+            return
+        }
+        #endif
+    }
+}
