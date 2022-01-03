b30852f95e1c29e9fcf225ad2b2e58b0d62c8ad6
yoshipjr <yoshihisa4413kitahara@gmail.com>
2022-01-03 13:43:39 +0900

    create search view controller

---
+++ MyLife/Presentation/SearchViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
