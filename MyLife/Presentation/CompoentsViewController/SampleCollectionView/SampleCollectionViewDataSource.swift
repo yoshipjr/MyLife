600b2c1349415b089e350270c274bbe497a8fe3e
yoshipjr <yoshihisa4413kitahara@gmail.com>
2022-01-09 23:09:26 +0900

    add delegate method

--- MyLife/Presentation/CompoentsViewController/SampleCollectionView/SampleCollectionViewDataSource.swift
+++ MyLife/Presentation/CompoentsViewController/SampleCollectionView/SampleCollectionViewDataSource.swift
@@ -19,18 +19,37 @@ final class SampleCollectionViewDataSource: NSObject {
 
 extension SampleCollectionViewDataSource: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        <#code#>
+        let cell = sections[indexPath.row].configureCell(collectionView, at: indexPath)
+
+        switch cell {
+            case is SampleCollectionViewCell:
+                break
+
+            default:
+                break
+        }
     }
 }
 
 extension SampleCollectionViewDataSource: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        sections.count
     }
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        sections[section].numberOfItems
     }
 
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = sections[indexPath.section].configureCell(collectionView, at: indexPath)
+        return cell
+    }
 
 }
